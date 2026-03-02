eab985c3950618d48aca41cf63a1bd94e3740d0d
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-03-02 12:50:54 +0530

    first commit

--- frontend/src/submit.js
+++ frontend/src/submit.js
@@ -7,26 +7,27 @@ export const SubmitButton = () => {
   const handleSubmit = async () => {
     try {
       const res = await fetch(
-        "https://pipeline-builder-tw15.onrender.com/parse",
+        "https://pipeline-builder-tw15.onrender.com/pipelines/parse",
         {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({ nodes, edges }),
-        },
+        }
       );
 
       const data = await res.json();
+      console.log("API RESPONSE:", data); // 🔍 debug
 
       alert(
         `Pipeline Info:
 Nodes: ${data.num_nodes}
 Edges: ${data.num_edges}
-Is DAG: ${data.is_dag}`,
+Is DAG: ${data.is_dag}`
       );
     } catch (error) {
-      console.error(error);
+      console.error("Error:", error);
       alert("Error connecting to backend");
     }
   };
